71ea74a5caea5dac041f4f107cc4a745908a9ce3
jsoltes <j.soltes@cubevision.cz>
2024-03-22 11:26:54 +0100

    Fix images db table

---
+++ migrations/Version20240322102532.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240322102532 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE images CHANGE x x INT DEFAULT NULL, CHANGE y y INT NOT NULL, CHANGE w w INT NOT NULL, CHANGE h h INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE images CHANGE x x DOUBLE PRECISION DEFAULT NULL, CHANGE y y DOUBLE PRECISION NOT NULL, CHANGE w w DOUBLE PRECISION NOT NULL, CHANGE h h DOUBLE PRECISION NOT NULL');
+    }
+}
