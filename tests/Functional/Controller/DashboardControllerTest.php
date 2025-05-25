7a0497bdcee8b93467e9b8ab4d44b2ce394f4175
jsoltes <j.soltes@cubevision.cz>
2025-05-26 00:33:05 +0200

    Fix text

--- tests/Functional/Controller/DashboardControllerTest.php
+++ tests/Functional/Controller/DashboardControllerTest.php
@@ -4,22 +4,24 @@ namespace App\Tests\Functional\Controller;
 
 use App\DataFixtures\AppFixtures;
 use App\Repository\UserRepository;
+use App\Test\Translation\TranslationHelperTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DashboardControllerTest extends WebTestCase
 {
+    use TranslationHelperTrait;
+
     public function testDashboardWorks(): void
     {
         $client = static::createClient();
-//        $user = self::$container->get(UserRepository::class)->findOneByEmail('admin@example.com');
         $user = $client->getContainer()->get(UserRepository::class)->findOneByEmail(AppFixtures::USER_1_EMAIL);
         $client->loginUser($user);
         $crawler = $client->request('GET', '/cs/1/dashboard');
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello DashBoardController!');
+        $this->assertSelectorTextContains('h1', $this->t('dashboard.overview'));
     }
 
-    public function testDashboardUnauthorizedAccessWorks(): void
+    public function testDashboardUnauthorizedAccessRedirect(): void
     {
         $client = static::createClient();
         $user = $client->getContainer()->get(UserRepository::class)->findOneByEmail(AppFixtures::USER_1_EMAIL);
@@ -27,7 +29,4 @@ class DashboardControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/cs/2/dashboard');
         $this->assertResponseRedirects('/cs/1/dashboard', 302);
     }
-
-    //UserDoesNotHaveAccessToCompany
-//;
 }
