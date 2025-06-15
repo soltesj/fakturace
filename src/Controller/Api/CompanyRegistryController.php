a538209ffeb61b8f1fdec213bb332a3fb9c30bce
jsoltes <j.soltes@cubevision.cz>
2025-06-15 19:57:52 +0200

    refactoring Symfony\Component\Routing\Attribute\Route => Symfony\Component\Routing\Attribute\Route

--- src/Controller/Api/CompanyRegistryController.php
+++ src/Controller/Api/CompanyRegistryController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\RateLimiter\RateLimiterFactory;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\DependencyInjection\Attribute\Target;
 
 
--- src/Controller/SendMailController.php
+++ src/Controller/SendMailController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\Exception\ExceptionInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_USER')]
